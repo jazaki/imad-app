174d725aa05427e5f2f6e6aeb3e860a8e626ef00
Junaid Arham <junaid_zaki@hotmail.com>
2017-09-09 18:58:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,13 +68,13 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res){
+/*app.get('/article-two',function(req,res){
     res.send('Article two is requested and will be served');
 });
 
 app.get('/article-three',function(req,res){
     res.send('Article threeis requested and will be served');
-});
+});*/
 //week-2 code ended
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
